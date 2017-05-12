e062deb0ed816008808359523aba730707439e7e
tony-elistratov <tony.elistratov@gmail.com>
2017-05-12 14:29:36 -0400

    at_home part added

---
+++ workshop1/at_home/tools.h
@@ -0,0 +1,12 @@
+#ifndef SICT_TOOLS_H_
+#define SICT_TOOLS_H_
+
+namespace sict {
+// Performs a fool-proof integer entry
+int getInt(int min, int max);
+
+// Displays the user interface menu
+int menu();
+
+}
+#endif
